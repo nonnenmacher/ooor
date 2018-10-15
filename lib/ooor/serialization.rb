23198593a8a17dd6a00a654d8d5de0d10e51ebb5
Vincent Nonnenmacher <vincent.nonnenmacher@gmail.com>
2018-10-15 13:36:11 +0200

    tentative fix for JSON embedded associations not being included as array of `named ERP class prefix' but as stray object (without type)

--- lib/ooor/serialization.rb
+++ lib/ooor/serialization.rb
@@ -29,6 +29,7 @@ module Ooor
       self.class.many2one_associations.keys.each do |k|
         if loaded_associations[k].is_a? Base
           included_associations[k] = loaded_associations[k].as_json[loaded_associations[k].class.openerp_model.gsub('.', '_')]
+          included_associations[k] = loaded_associations[k].as_json
         elsif associations[k].is_a? Array
           included_associations[k] = {"id" => associations[k][0], "name" => associations[k][1]}
         end
@@ -38,7 +39,8 @@ module Ooor
     def serialize_x_to_many(included_associations)
       (self.class.one2many_associations.keys + self.class.many2many_associations.keys).each do |k|
         if loaded_associations[k].is_a? Array
-          included_associations[k] = loaded_associations[k].map {|item| item.as_json[item.class.openerp_model.gsub('.', '_')]}
+					#included_associations[k] = loaded_associations[k].map {|item| item.as_json[item.class.openerp_model.gsub('.', '_')]}
+          included_associations[k] = loaded_associations[k].map {|item| item.as_json}
         else
           included_associations[k] = associations[k].map {|id| {"id" => id}} if associations[k]
         end
