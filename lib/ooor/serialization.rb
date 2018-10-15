c73ed5750307a59e70c7ecff18f1721392e13be9
Vincent Nonnenmacher <vincent.nonnenmacher@gmail.com>
2018-10-15 13:37:03 +0200

    better/speedier code (check this)

--- lib/ooor/serialization.rb
+++ lib/ooor/serialization.rb
@@ -22,13 +22,17 @@ module Ooor
       serialize_x_to_many(included_associations)
 
       method_names = Array.wrap(options[:methods]).map { |n| n if respond_to?(n.to_s) }.compact
-      Hash[(attribute_names + method_names).map { |n| [n, send(n)] }].merge(included_associations)
+			unless method_names.empty?
+			else
+				hash.merge(Hash[method_names.map { |n| [n, send(n)] }])
+			end
+			hash.merge(included_associations)
     end
 
     def serialize_many2one(included_associations)
       self.class.many2one_associations.keys.each do |k|
         if loaded_associations[k].is_a? Base
-          included_associations[k] = loaded_associations[k].as_json[loaded_associations[k].class.openerp_model.gsub('.', '_')]
+					included_associations[k] = loaded_associations[k].as_json[loaded_associations[k].class.openerp_model.gsub('.', '_')]
           included_associations[k] = loaded_associations[k].as_json
         elsif associations[k].is_a? Array
           included_associations[k] = {"id" => associations[k][0], "name" => associations[k][1]}
